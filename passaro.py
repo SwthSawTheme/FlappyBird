27c378fc46b04bfccd9dc4880eab463f9c8a0758
SwthSawTheme <sawtheme1@gmail.com>
2024-05-19 11:11:35 -0300

    class Passaro, adicionado metodo mover

--- passaro.py
+++ passaro.py
@@ -22,3 +22,21 @@ class Passaro:
         self.velocidade = -10.5
         self.tempo = 0
         self.altura = self.y
+        
+    def mover(self):
+        #calcular o deslocamento
+        self.tempo += 1
+        deslocamento = 1.5 * (self.tempo**2) + self.velocidade * self.tempo
+        #restringir o deslocamento
+        if deslocamento > 16:
+            deslocamento = 16
+        elif deslocamento < 0:
+            deslocamento -= 2
+        self.y += deslocamento
+        #angulo do passaro
+        if deslocamento < 0  or self.y < (self.altura + 50):
+            if self.angulo < self.ROTAÇÃO_MAXIMA:
+                self.angulo = self.ROTAÇÃO_MAXIMA
+            else:
+                if self.angulo > -90:
+                    self.angulo -= self.VLOCIDADE_ROTAÇÃO
